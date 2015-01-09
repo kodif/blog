33adf55d710d7327f815d18eabb7823cdc45ba97
Ian Monge <ian.monge@socialpoint.es>
2015-01-09 13:12:53 +0100

    Executed command 'bin/behat --suite=kodify_suite --dry-run --append-snippets'

--- features/bootstrap/Kodify/BlogBundle/Features/Context/FeatureContext.php
+++ features/bootstrap/Kodify/BlogBundle/Features/Context/FeatureContext.php
@@ -2,6 +2,7 @@
 
 namespace Kodify\BlogBundle\Features\Context;
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
@@ -15,4 +16,52 @@ class FeatureContext implements Context, SnippetAcceptingContext
 {
     use KernelDictionary;
 
+
+    /**
+     * @Given the following authors exist:
+     */
+    public function theFollowingAuthorsExist(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given the following posts exist:
+     */
+    public function theFollowingPostsExist(TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given I visit the home page
+     */
+    public function iVisitTheHomePage()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then The post with title :arg1 is on first column, first row
+     */
+    public function thePostWithTitleIsOnFirstColumnFirstRow($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then The post with title :arg1 is on the second column, first row
+     */
+    public function thePostWithTitleIsOnTheSecondColumnFirstRow($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then The post with title :arg1 is on the first column, second row
+     */
+    public function thePostWithTitleIsOnTheFirstColumnSecondRow($arg1)
+    {
+        throw new PendingException();
+    }
 }
